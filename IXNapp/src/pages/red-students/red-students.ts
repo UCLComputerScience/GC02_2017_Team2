a3a894d3ad0cca74bdfbdcb227c674c2151960da
antoine186 <antoine186@hotmail.com>
2017-11-16 14:56:03 +0000

    blabla

--- IXNapp/src/pages/red-students/red-students.ts
+++ IXNapp/src/pages/red-students/red-students.ts
@@ -5,10 +5,10 @@ import { NavController } from 'ionic-angular';
   selector: 'page-red-students',
   templateUrl: 'red-students.html'
 })
+
 export class RedStudentsPage {
-  // this tells the tabs component which Pages
-  // should be each tab's root Page
+
   constructor(public navCtrl: NavController) {
   }
-  
+
 }
