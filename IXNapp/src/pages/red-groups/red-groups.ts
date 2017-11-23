08900cc49279c152bccf928eb47766e45be2f380
Wontek <wontekh@gmail.com>
2017-11-23 16:57:32 +0000

    Change Red

--- IXNapp/src/pages/red-groups/red-groups.ts
+++ IXNapp/src/pages/red-groups/red-groups.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-red-groups',
   templateUrl: 'red-groups.html'
 })
-export class RedGroupsPage {
+export class RedGroupsPage implements OnInit {
+
+RedWeeks: any[];
+RedGroups: string[];
 
   constructor(public navCtrl: NavController) {
   }
   
+ngOnInit() {
+	
+	this.RedGroups = ["G1", "G2", "G3", "G4"];
+}
+
+onClick() {
+	this.RedWeeks = ["Week 1", "Week 2", "Week 3", "Week 4", "Week 5"];
+}
+
 }
--- IXNapp/src/pages/red-students/red-students.ts
+++ IXNapp/src/pages/red-students/red-students.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +6,22 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'red-students.html'
 })
 
-export class RedStudentsPage {
+export class RedStudentsPage implements OnInit {
+
+RedWeeks: any[];
+RedStudents: String[];
+	
 
   constructor(public navCtrl: NavController) {
   }
+
+ngOnInit() {
+
+	this.RedStudents = ["Gary Johnson", "Abigail Taylor", "Tony Whatever", "Gideon Hacquah", ];
+}
+
+onClick() {
+	this.RedWeeks = [ {Week: "Week 1", Contr: "30%"}, {Week: "Week 2", Contr: "29%"}, {Week: "Week 3", Contr: "33%"}];
+}
 
 }
