d3eff166434dc374b9bf565484abe6398dba4e9d
antoine186 <33133769+antoine186@users.noreply.github.com>
2017-11-26 19:45:34 +0000

    Update red-students.ts

--- IXNapp/src/pages/red-students/red-students.ts
+++ IXNapp/src/pages/red-students/red-students.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'page-red-students',
   templateUrl: 'red-students.html'
+
 })
 
 export class RedStudentsPage implements OnInit {
@@ -23,8 +25,21 @@ ngOnInit() {
 }
 
 onClick() {
-	this.RedWeeks = [ {Week: "Week 1", Contr: "30%"}, {Week: "Week 2", Contr: "29%"}, {Week: "Week 3", Contr: "33%"}];
+	this.RedWeeks = [ {Week: "Week 1", Color: 2, Contr: "30%"}, {Week: "Week 2", Color: 3, Contr: "29%"}, {Week: "Week 3", Color: 1, Contr: "33%"}];
+	for(let i = 0; i < this.RedWeeks.length; i++){
+		if(this.RedWeeks[i].Color == 4){
+			this.RedWeeks[i].Color = "dark";
+		} else if(this.RedWeeks[i].Color == 3){
+			this.RedWeeks[i].Color = "light";
+		} else if(this.RedWeeks[i].Color == 2){
+			this.RedWeeks[i].Color = "secondary";
+		} else if(this.RedWeeks[i].Color == 1){
+			this.RedWeeks[i].Color = "danger";
+		}
+	}
 	this.Contribution = ["Contribution"];
 }
+
+
 
 }
