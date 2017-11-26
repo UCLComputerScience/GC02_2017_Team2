e241e3b139d2162a47576c640030886470ba5f7a
antoine186 <33133769+antoine186@users.noreply.github.com>
2017-11-26 19:41:48 +0000

    Update staff-contact-info.ts

--- IXNapp/src/pages/staff-contact-info/staff-contact-info.ts
+++ IXNapp/src/pages/staff-contact-info/staff-contact-info.ts
@@ -16,9 +16,9 @@ TAContact: any[];
 ngOnInit() {
 	
 	this.StaffContact= [{Name: "Yun Fu", Email: "yunfu@ucl.ac.uk", Hours: "13:00", Location: "Malet Place"}];
-	this.TAContact= [{Name: "John Taylor", Email: "John@ucl.ac.uk", Hours: "14:00", Location: "Malet Place", Exp: "Ionic"}, {Name: "Dominic Twain", Email: "Dominic@ucl.ac.uk", Hours: "16:00", Location: "Malet Place", Exp: "Databases"}, {Name: "Ashley Shana", Email: "Ashley@ucl.ac.uk", Hours: "14:00", Location: "Malet Place", Exp: "Python"}];
+
+	this.TAContact= [{Name: "John Taylor", Email: "John@ucl.ac.uk", Hours: "14:00", Location: "Malet Place", Exp: "Ionic", Pic: "assets/img/TA3.png"}, {Name: "Dominic Twain", Email: "Dominic@ucl.ac.uk", Hours: "16:00", Location: "Malet Place", Exp: "Databases", Pic: "assets/img/TA2.png"}, {Name: "Ashley Shana", Email: "Ashley@ucl.ac.uk", Hours: "14:00", Location: "Malet Place", Exp: "Python", Pic: "assets/img/TA1.png"}];
 
 }
 
 }
-
