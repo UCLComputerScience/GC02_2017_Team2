a9b9190ce915ec338bedf1ee05d2bd64d5f1b502
Wontek <wontekh@gmail.com>
2017-11-29 13:28:13 +0000

    HomePage

--- IXNapp/src/pages/home/home.ts
+++ IXNapp/src/pages/home/home.ts
@@ -7,117 +7,127 @@ import { Chart } from 'chart.js';
 import { Slides } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
-  @ViewChild('doughnutCanvas') doughnutCanvas;
-  @ViewChild('doughnutCanvas2') doughnutCanvas2;
-  @ViewChild(Slides) slides: Slides;
+    @ViewChild('doughnutCanvas') doughnutCanvas;
+    @ViewChild('doughnutCanvas2') doughnutCanvas2;
+    @ViewChild(Slides) slides: Slides;
 
-  ngAfterViewInit() {
-    console.log("Slider------------>>Input Props Set");
-    this.slides.autoHeight = true;
-    // this.Slides.observer = true ;
-  }
+    ngAfterViewInit() {
+        console.log("Slider------------>>Input Props Set");
+        this.slides.autoHeight = true;
+        // this.Slides.observer = true ;
+    }
 
-  innerHeight: any; 
-  innerWidth: any; 
-  doughnutChart: any;
-  constructor(public navCtrl: NavController) {
-    this.innerHeight = (window.screen.height);  
-    this.innerWidth = (window.screen.width); //+"px"
-  }
-  goToDeadlines(params){
-    if (!params) params = {};
-    this.navCtrl.push(DeadlinesPage);
-  }goToRedGroups(params){
-    if (!params) params = {};
-    this.navCtrl.push(RedGroupsPage);
-  }goToRedStudents(params){
-    if (!params) params = {};
-    this.navCtrl.push(RedStudentsPage);
-  }
+    innerHeight: any;
+    innerWidth: any;
+    doughnutChart: any;
+    Fred: any;
+    Fyellow: any;
+    Fgreen: any;
+    Fdgreen: any;
 
-  ionViewDidLoad() {    
-    let donutCtx = this.doughnutCanvas.nativeElement; 
-    donutCtx.height = innerHeight*0.3; 
-    var datax = [4, 5, 6, 8]; 
-    var data = {
-        labels: datax,
-        datasets: [
+    constructor(public navCtrl: NavController) {
+        this.innerHeight = (window.screen.height);
+        this.innerWidth = (window.screen.width); //+"px"
+    }
+    goToDeadlines(params) {
+        if (!params) params = {};
+        this.navCtrl.push(DeadlinesPage);
+    } goToRedGroups(params) {
+        if (!params) params = {};
+        this.navCtrl.push(RedGroupsPage);
+    } goToRedStudents(params) {
+        if (!params) params = {};
+        this.navCtrl.push(RedStudentsPage);
+    }
+
+    ionViewDidLoad() {
+        this.Fred = 4;
+        this.Fyellow = 6;
+        this.Fgreen = 5;
+        this.Fdgreen = 7;
+
+        let donutCtx = this.doughnutCanvas.nativeElement;
+        donutCtx.height = innerHeight * 0.28;
+        var datax = [4, 5, 6, 8];
+        var data = {
+            labels: datax,
+            datasets: [
+                {
+                    "data": datax,
+                    "backgroundColor": [
+                        "#ff6384", //red
+                        "#ffcd56", //yellow
+                        "#7ed321", //light gree
+                        "#058d65" //dark green 
+                    ]
+                }]
+        };
+
+        new Chart(
+            donutCtx,
             {
-                "data": datax,
-                "backgroundColor": [ 
-                    "#ff6384", //red
-                    "#ffcd56", //yellow
-                    "#7ed321", //light gree
-                    "#058d65" //dark green 
-                ]
-            }]
-    };
+                "type": 'doughnut',
+                "data": data,
+                "options": {
+                    legend: {
+                        display: false,
+                        position: 'right',
+                    },
+                    responsive: true,
+                    maintainAspectRatio: true,
+                    "animation": {
+                        "animateScale": true,
+                        "animateRotate": false
+                    },
 
-    new Chart(
-        donutCtx,
-        {
-            "type": 'doughnut',
-            "data": data,
-            "options": {
-                legend: { 
-                    display: false, 
-                    position: 'right',
-                }, 
-                responsive: true,
-                maintainAspectRatio: true,
-                "animation": {
-                    "animateScale": true,
-                    "animateRotate": false
-                }, 
-              
-            }
-        }
-    );
-
-    let donutCtx2 = this.doughnutCanvas2.nativeElement; 
-    donutCtx2.height = innerHeight*0.3; 
-
-    var data2 = {
-        labels: [
-            "Excellent",
-            "Good",
-            "Average", 
-            "Bad"
-        ],
-        datasets: [
-            {
-                "data": [4, 5, 6, 3],
-                "backgroundColor": [ 
-                    "#ff6384", //red
-                    "#ffcd56", //yellow
-                    "#7ed321", //light gree
-                    "#058d65" //dark green 
-                ]
-            }]
-    };
-
-    new Chart(
-        donutCtx2,
-        {
-            "type": 'doughnut',
-            "data": data2,
-            "options": {
-                legend: { 
-                    display: false, 
-                    position: 'right', 
-                }, 
-                responsive: true,
-                maintainAspectRatio: true,
-                "animation": {
-                    "animateScale": true,
-                    "animateRotate": false
                 }
             }
-        }
-    );
+        );
+
+        let donutCtx2 = this.doughnutCanvas2.nativeElement;
+        donutCtx2.height = innerHeight * 0.28;
+
+        var data2 = {
+            labels: [
+                "Excellent",
+                "Good",
+                "Average",
+                "Bad"
+            ],
+            datasets: [
+                {
+                    "data": [4, 5, 6, 3],
+                    "backgroundColor": [
+                        "#ff6384", //red
+                        "#ffcd56", //yellow
+                        "#7ed321", //light gree
+                        "#058d65" //dark green 
+                    ]
+                }]
+        };
+
+        new Chart(
+            donutCtx2,
+            {
+                "type": 'doughnut',
+                "data": data2,
+                "options": {
+                    legend: {
+                        display: false,
+                        position: 'right',
+                    },
+                    responsive: true,
+                    maintainAspectRatio: true,
+                    "animation": {
+                        "animateScale": true,
+                        "animateRotate": false
+                    }
+                }
+            }
+        );
     }
 }
