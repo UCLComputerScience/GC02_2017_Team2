cc422b9b61ea8f5961164b0aedc27357f0375561
Wontek <wontekh@gmail.com>
2017-12-05 01:36:02 +0000

    Deadlines

--- IXNapp/src/pages/deadlines/deadlines.ts
+++ IXNapp/src/pages/deadlines/deadlines.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-deadlines',
   templateUrl: 'deadlines.html'
 })
+
 export class DeadlinesPage {
-  // this tells the tabs component which Pages
-  // should be each tab's root Page
+
+  Deadlinetitles: any[];
+  Deadlinedates: any[];
   constructor(public navCtrl: NavController) {
   }
-  
+
+  ngOnInit(){
+
+    this.Deadlinetitles= [{Title: "HCI Deadline", Date: "15/11/2017"},{Title: "Final Deadline", Date: "10/01/2018"}];
+    this.Deadlinedates= [{Date: "15/11/2017"}, {Date: "10/01/2018"}];
+  }
+
+  Date(): String{
+      var today = new Date();
+      var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+      return date;
+  }
+
 }
