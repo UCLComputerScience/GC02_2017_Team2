8c0827d1875d85378ea31cff8573163ffef19eda
antoine186 <33133769+antoine186@users.noreply.github.com>
2017-12-11 04:03:43 +0000

    Update red-groups.ts

--- IXNapp/src/pages/red-groups/red-groups.ts
+++ IXNapp/src/pages/red-groups/red-groups.ts
@@ -1,36 +1,61 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NgStyle } from '@angular/common';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'page-red-groups',
   templateUrl: 'red-groups.html'
 })
-export class RedGroupsPage implements OnInit {
+export class RedGroupsPage {
 
+data:any = {};
+data2:any = {};
 RedWeeks: any[];
-RedGroups: string[];
+RedGroups: object[];
+TAID: number;
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public http: Http, public http2 : Http) {
+  this.TAID = 1;
+  this.getGroupNames(this.TAID);
+
   }
-  
-ngOnInit() {
-	
-	this.RedGroups = ["G1", "G2", "G3", "G4"];
-}
 
-onClick() {
-	this.RedWeeks = [{Week: "Week 1", Color: 4}, {Week: "Week 2", Color: 1}, {Week: "Week 3", Color: 1}, {Week: "Week 4", Color: 2}, {Week: "Week 5", Color: 1}];
-	for(let i = 0; i < this.RedWeeks.length; i++){
-		if(this.RedWeeks[i].Color == 4){
-			this.RedWeeks[i].Color = "dark";
-		} else if(this.RedWeeks[i].Color == 3){
-			this.RedWeeks[i].Color = "light";
-		} else if(this.RedWeeks[i].Color == 2){
-			this.RedWeeks[i].Color = "secondary";
-		} else if(this.RedWeeks[i].Color == 1){
-			this.RedWeeks[i].Color = "danger";
+  getGroupNames(TAID: number) {
+ 	var link1 = 'http://gc02team02app.azurewebsites.net/SQL/RedGroupsInit.php/';
+	var myData1 = JSON.stringify({teachID: TAID})
+
+ 	this.http2.post(link1, myData1).subscribe(data2 => {
+		this.RedGroups = JSON.parse(data2["_body"]);   /* data["_body"] */
+		console.log(this.RedGroups);
+	}, error => {
+		console.log("Oooooops!");
+	});
+ }
+
+ onClick(G : any) {
+	console.log(G);
+
+	var link = 'http://gc02team02app.azurewebsites.net/SQL/RedGroupReceive.php/';
+	var myData = JSON.stringify({grID: G})
+
+	this.http.post(link, myData).subscribe(data => {
+		this.RedWeeks = JSON.parse(data["_body"]);   /* data["_body"] */
+		for(let i = 0; i < this.RedWeeks.length; i++){
+		if(this.RedWeeks[i].g_fb == 4){
+			this.RedWeeks[i].g_fb = "dark";
+		} else if(this.RedWeeks[i].g_fb == 3){
+			this.RedWeeks[i].g_fb = "light";
+		} else if(this.RedWeeks[i].g_fb == 2){
+			this.RedWeeks[i].g_fb = "secondary";
+		} else if(this.RedWeeks[i].g_fb == 1){
+			this.RedWeeks[i].g_fb = "danger";
 		}
 	}
-}
+		console.log(this.RedWeeks);
+	}, error => {
+		console.log("Oooooops!");
+	});
+ 	}
 
 }
