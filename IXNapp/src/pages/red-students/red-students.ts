02440ea980d28b8222d1eee8884d3ccccae55daf
antoine186 <33133769+antoine186@users.noreply.github.com>
2017-12-22 21:23:24 +0000

    Update red-students.ts

--- IXNapp/src/pages/red-students/red-students.ts
+++ IXNapp/src/pages/red-students/red-students.ts
@@ -3,7 +3,7 @@ import { NavController } from 'ionic-angular';
 import { NgStyle } from '@angular/common';
 import { Http, Headers, RequestOptions } from '@angular/http';
 
-import { RedStudantProvider } from '../../providers/red-studant/red-studant'
+import { HouseProvider } from '../../providers/house/house';
 
 @Component({
   selector: 'page-red-students',
@@ -13,61 +13,62 @@ import { RedStudantProvider } from '../../providers/red-studant/red-studant'
 
 export class RedStudentsPage {
 
-data:any = {};
-data2:any = {};
-Students: object[];
-RedWeeks: any[];
-Contribution: any[];
-TAID: number;
+dt:any = {};
+dt2:any = {};
+wkn: number[] = [];
+mx: number;
+clicked: any = false;
+Students: any[];
+RStudents: any[] = [];
+Weeks: any[];
+RWeeks: any[] = [];
 	
 
-  constructor(public navCtrl: NavController, public redstudantprovider: RedStudantProvider, public http: Http, public http2 : Http) {
-  	this.data.fname = '';
-  	this.data.lname = '';
-  	this.data.response = '';
-  	this.TAID = 1;
-  	this.getStudentNames(this.TAID);
+  constructor(public navCtrl: NavController, public house: HouseProvider, public http: Http, public http2 : Http) {
 
   }
 
+  ngOnInit() {
+  	this.house.getAllRedStudent().subscribe(dt => {
+  		this.Students = JSON.parse(dt["_body"]);
 
- getStudentNames(TAID: number) {
- 	var link1 = 'http://gc02team02app.azurewebsites.net/SQL/RedStudentInit.php/';
-	var myData1 = JSON.stringify({teachID: TAID})
+  		for(let i in this.Students){
+  		this.wkn.push(this.Students[i].weeknum);
+  		}
+  		this.mx = Math.max.apply(Math, this.wkn);
 
- 	this.http2.post(link1, myData1).subscribe(data2 => {
-		this.Students = JSON.parse(data2["_body"]);   /* data["_body"] */
-		console.log(this.Students);
-	}, error => {
-		console.log("Oooooops!");
-	});
- }
+  		for(let k in this.Students){
+  		if(this.Students[k].weeknum == this.mx && this.Students[k].st_fb == 1) {
+  			this.RStudents.push(this.Students[k]);
+  		}
+  		}
 
+  	})
+  } 
 
 onClick(first : any, last: any) {
-	console.log(first);
-	console.log(last);
+	this.RWeeks = [];
+	this.clicked = true;
 
-	var link = 'http://gc02team02app.azurewebsites.net/SQL/RedStudentReceive.php/';
-	var myData = JSON.stringify({studentfirstname: first, studentlastname: last})
-
-	this.http.post(link, myData).subscribe(data => {
-		this.RedWeeks = JSON.parse(data["_body"]);   /* data["_body"] */
-		for(let i = 0; i < this.RedWeeks.length; i++){
-		if(this.RedWeeks[i].st_fb == 4){
-			this.RedWeeks[i].st_fb = "dark";
-		} else if(this.RedWeeks[i].st_fb == 3){
-			this.RedWeeks[i].st_fb = "light";
-		} else if(this.RedWeeks[i].st_fb == 2){
-			this.RedWeeks[i].st_fb = "secondary";
-		} else if(this.RedWeeks[i].st_fb == 1){
-			this.RedWeeks[i].st_fb = "danger";
+this.house.getAllRedStudent().subscribe(dt2 => {
+	this.Weeks = JSON.parse(dt2["_body"]);
+	for(let i in this.Weeks){
+	if(this.Weeks[i].first == first && this.Weeks[i].last == last){
+		this.RWeeks.push(this.Weeks[i]);
+	}
+	}
+	for(let i = 0; i < this.RWeeks.length; i++){
+		if(this.RWeeks[i].st_fb == 4){
+			this.RWeeks[i].st_fb = "dark";
+		} else if(this.RWeeks[i].st_fb == 3){
+			this.RWeeks[i].st_fb = "light";
+		} else if(this.RWeeks[i].st_fb == 2){
+			this.RWeeks[i].st_fb = "secondary";
+		} else if(this.RWeeks[i].st_fb == 1){
+			this.RWeeks[i].st_fb = "danger";
 		}
 	}
-		console.log(this.RedWeeks);
-	}, error => {
-		console.log("Oooooops!");
-	});
-this.Contribution = ["Contribution"];
+})
+
  	}
 }
