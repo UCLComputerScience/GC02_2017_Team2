c703ee92a7f76d1719925c601af307b8c4599c92
Sophia <sophia.botz@gmail.com>
2017-12-26 22:18:17 +0100

    updates profile, more details and groups page

--- IXNapp/src/pages/more-details/more-details.ts
+++ IXNapp/src/pages/more-details/more-details.ts
@@ -8,16 +8,30 @@ import { NavController } from 'ionic-angular';
 export class MoreDetailsPage {
     constructor(public navCtrl: NavController) {
     }
-
+    
+    //BACKEND HERE!!
+    group = 5;
+    week = 4;
     names = ['Samantha Watson', 'John Taylor', 'James Kirk'];
-    individualPerformances = ['good','good','average'];
+    individualPerformances = [4, 4, 5];
     individualComments = ['good work','',''];
-    
-    groupPerformance = 'average';
-    
+    individualContributions = [33, 34, 33];
+    groupPerformance = 2;
     groupComment = 'The group progressed from last week but needs to work on the prototype more';
+    
+    values = ['bad', 'average', 'good', 'excellent']; // don't change
+    groupPerformance2 = this.getGroupPerformance();
 
-
+    public getPerformance(x) {
+    var performance = this.individualPerformances[x];
+    var value = this.values[performance-1]; 
+    return value; 
+    }
+  
+    getGroupPerformance() {
+        var value = this.values[this.groupPerformance-1];
+        return value;
+    }
 
 
 
