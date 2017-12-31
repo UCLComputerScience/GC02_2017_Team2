91cd5188cbd9ff9c46f367bd0570e20b233b43ba
antoine186 <33133769+antoine186@users.noreply.github.com>
2017-12-31 04:54:07 +0000

    Update house.ts

--- IXNapp/src/providers/house/house.ts
+++ IXNapp/src/providers/house/house.ts
@@ -8,26 +8,24 @@ import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { Subject } from 'rxjs/Subject';
 import { Http, Headers, RequestOptions } from '@angular/http';
-import { ShareService } from '../../providers/share'
 
 
 @Injectable()
 export class HouseProvider {
 
-	TAID: number = 1; /* I assume I know this here */
-  SID: number = 1;
-  
+	//TAID: number = 1; /* I assume I know this here */
+  	//SID: number = 1;
 
-  constructor(public http: Http, public http2: Http, public http3: Http, public http4: Http, public http5: Http, public http6: Http, public shareService: ShareService) {
+  TAID: number;
+  SID: number;
+
+  constructor(public http: Http, public http2: Http, public http3: Http, public http4: Http, public http5: Http, public http6: Http) {
     console.log('Hello HouseProvider Provider');
   }
 
-  
-
   getAllRedStudent() {
 
-  this.TAID = this.shareService.getUserID();   
-  var link1 = 'http://gc02team02app.azurewebsites.net/SQL/RedStudentInit.php/';
+  	var link1 = 'http://gc02team02app.azurewebsites.net/SQL/RedStudentInit.php/';
 	var myData1 = JSON.stringify({teachID: this.TAID})
 
  	return this.http.post(link1, myData1);
@@ -35,7 +33,6 @@ export class HouseProvider {
   }
 
   getAllRedTeam() {
-    this.TAID = this.shareService.getUserID(); 
   	var link2 = 'http://gc02team02app.azurewebsites.net/SQL/RedGroupsInit.php';
 	var myData2 = JSON.stringify({teachID: this.TAID})
 
@@ -43,7 +40,6 @@ export class HouseProvider {
   }
 
   GetStage2Student() {
-    this.TAID = this.shareService.getUserID(); 
     	var link3 = 'http://gc02team02app.azurewebsites.net/SQL/Stage2StudentPrelim.php';
   	var myData3 = JSON.stringify({teachID: this.TAID})
 
@@ -56,7 +52,6 @@ export class HouseProvider {
   }
 
   GetStudentAdd() {
-    this.TAID = this.shareService.getUserID(); 
       var link4 = 'http://gc02team02app.azurewebsites.net/SQL/Stage2StudentAdd.php';
     var myData4 = JSON.stringify({teachID: this.TAID})
 
@@ -64,12 +59,20 @@ export class HouseProvider {
   }
 
   GetStudentHome() {
-    this.SID = this.shareService.getUserID(); 
       var link5 = 'http://gc02team02app.azurewebsites.net/SQL/StudentHome.php';
     var myData5 = JSON.stringify({studID: this.SID})
 
     return this.http6.post(link5, myData5);
   }
 
+  setSID(userID){
+       this.SID = userID; 
+   }
+
+  setTAID(userID){
+       this.TAID = userID; 
+   }
+
 
 }
+
