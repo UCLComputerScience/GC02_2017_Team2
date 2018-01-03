70e1c0cea1cec1673c01bf43bb0f274925d44d72
antoine186 <33133769+antoine186@users.noreply.github.com>
2018-01-03 14:19:38 +0000

    Update deadlines.ts

--- IXNapp/src/pages/deadlines/deadlines.ts
+++ IXNapp/src/pages/deadlines/deadlines.ts
@@ -10,7 +10,8 @@ import { Http, Headers, RequestOptions } from '@angular/http';
 
 export class DeadlinesPage {
 
-  Deadlinetitles: any[];
+  Deadlinetitles: any[] = [];
+  Deadlineraw: any[] = [];
 
 
   constructor(public navCtrl: NavController, public house: HouseProvider) {
@@ -19,14 +20,38 @@ export class DeadlinesPage {
 
   ngOnInit(){
 
-  this.house.getDead().subscribe(dt =>
-      this.Deadlinetitles = dt);
+  this.house.getDead().subscribe(dt => {
+      this.Deadlineraw = dt;
+
+      this.Deadlineraw.sort(function(a, b){
+      var aa = a.deadd.split('/').reverse().join();
+      var bb = b.deadd.split('/').reverse().join();
+      return aa < bb ? -1 : (aa > bb ? 1 : 0);
+      });
+
+      var today = new Date();
+      var d = today.getDate();
+      var m = today.getMonth()+1;
+      var y = today.getFullYear();
+      var dd: string = d + '/' + m + '/' + y; 
+      var parts = dd.split("/");
+      var z: Date = new Date(parseInt(parts[2]), (parseInt(parts[1]) - 1), parseInt(parts[0]));
+
+      for(let i in this.Deadlineraw) {
+      var t = this.Deadlineraw[i].deadd.split("/");
+      var w: Date = new Date(parseInt(t[2]), (parseInt(t[1]) - 1), parseInt(t[0]));
+        if(z < w) {
+          this.Deadlinetitles.push(this.Deadlineraw[i]);
+        }
+      }
+
+      })
 
   }
 
   Date(): String{
       var today = new Date();
-      var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+      var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
       return date;
   }
 
