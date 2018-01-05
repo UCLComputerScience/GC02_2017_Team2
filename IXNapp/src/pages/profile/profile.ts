9676d9ffd6154f8ad795e2e8ec98c125148170bc
Sophia <sophia.botz@gmail.com>
2018-01-05 14:06:12 +0100

    student and staff profile pages

--- IXNapp/src/pages/profile/profile.ts
+++ IXNapp/src/pages/profile/profile.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+import { LoginPage } from '../login/login';
+import { App } from 'ionic-angular';
 
 @Component({
   selector: 'page-profile',
@@ -8,6 +11,60 @@ import { NavController } from 'ionic-angular';
 export class ProfilePage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
-  constructor(public navCtrl: NavController) {
+  
+  firstName = '';
+  lastName = '';
+  email = '';
+  type = '';
+  password;
+  
+  constructor(private app: App, public navCtrl: NavController, private auth: AuthService, private alertCtrl: AlertController) {
+    let info = this.auth.getUserInfo();
+    this.firstName = info['firstName'];
+    this.lastName = info['lastName'];
+    this.email = info['email'];
+    this.type = info['type'];
   }
+  
+  public logout() {
+    this.auth.logout().subscribe(succ => {
+      this.app.getRootNav().setRoot(LoginPage);
+    });
+  }
+  
+  showError(text) {
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
+  
+  changePassword() {
+    if (this.password === null || this.password == null) {
+        this.showError("No password inserted");
+    }
+    else if (this.password !== null) {
+        if (this.password.length < 6) {
+            this.showError("Please choose a password that is at least 6 characters long");
+        }
+        else {
+            // BACKEND HERE TO SAVE PASSWORD
+            let alert = this.alertCtrl.create({
+              title: 'Success',
+              subTitle: 'Your password was changed',
+              buttons: ['OK']
+            });
+            alert.present(prompt);
+            this.password = null;
+        }
+    }
+    
+  }
+  
+  //BACKEND FOR STUDENT GROUP AND GROUP DESCRIPTION
+  myGroup = 'G3';
+  groupDescription = 'my Group Description';
+
 }
