a814efec2a73ea146e1fb16906b3d97c273d19ec
antoine186 <33133769+antoine186@users.noreply.github.com>
2018-01-06 06:44:47 +0000

    Update red-students.ts

--- IXNapp/src/pages/red-students/red-students.ts
+++ IXNapp/src/pages/red-students/red-students.ts
@@ -23,10 +23,11 @@ RStudents: any[] = [];
 Weeks: any[];
 RWeeks: any[] = [];
 RIWeeks: any[] = [];
+Message: string;
 	
 
   constructor(public navCtrl: NavController, public house: HouseProvider, public http: Http, public http2 : Http) {
-
+  this.Message = 'Press on a Student!';
   }
 
   ngOnInit() {
@@ -44,13 +45,58 @@ RIWeeks: any[] = [];
   		}
   		}
 
+      if(this.RStudents.length == 0) {
+        this.Message = 'No Red Students!';
+      }
+
   	})
+  }
+
+  doRefresh(refresher) {
+    console.log('Begin async operation', refresher);
+
+    this.Message = 'Press on a Student!';
+
+    this.dt = {};
+    this.wkn = [];
+    this.mx = 0;
+    this.clicked = false;
+    this.Students = [];
+    this.RStudents = [];
+    this.RWeeks = [];
+    this.RIWeeks = [];
+
+    this.house.getAllRedStudent().subscribe(dt => {
+      this.Students = JSON.parse(dt["_body"]);
+
+      for(let i in this.Students){
+      this.wkn.push(this.Students[i].weeknum);
+      }
+      this.mx = Math.max.apply(Math, this.wkn);
+
+      for(let k in this.Students){
+      if(this.Students[k].weeknum == this.mx && this.Students[k].st_fb == 1) {
+        this.RStudents.push(this.Students[k]);
+      }
+      }
+
+      if(this.RStudents.length == 0) {
+        this.Message = 'No Red Students!';
+      }
+
+    })
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      refresher.complete();
+    }, 2000);
   } 
 
 onClick(first : any, last: any) {
 	this.RWeeks = [];
 	this.RIWeeks = [];
 	this.clicked = true;
+  this.Weeks = [];
 
 this.house.getAllRedStudent().subscribe(dt2 => {
 	this.Weeks = JSON.parse(dt2["_body"]);
